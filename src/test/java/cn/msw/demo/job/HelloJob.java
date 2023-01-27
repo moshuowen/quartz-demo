596f9988a01fc4fa07e9910077d65bfd985bea49
moshuowen <2083643622@qq.com>
2023-01-27 22:33:02 +0800

    添加注释

--- src/test/java/cn/msw/demo/job/HelloJob.java
+++ src/test/java/cn/msw/demo/job/HelloJob.java
@@ -1,12 +1,32 @@
 package cn.msw.demo.job;
 
 import org.quartz.Job;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.Date;
+
 public class HelloJob implements Job {
+    /**
+     * 执行任务
+     * @param context 任务上下文
+     * @throws JobExecutionException 任务执行异常
+     */
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("hello job");
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        // 获取任务详情
+        JobDetail jobDetail = context.getJobDetail();
+
+        // 获取任务名称
+        String jobName = jobDetail.getKey().getName();
+
+        // 获取任务组名
+        String jobGroup = jobDetail.getKey().getGroup();
+        // 打印
+        System.out.println("jobName: " + jobName + ", jobGroup: " + jobGroup);
+        System.out.println(new Date());
+
+
     }
 }
--- src/test/java/cn/msw/demo/job/Main.java
+++ src/test/java/cn/msw/demo/job/Main.java
@@ -6,19 +6,20 @@ import org.quartz.impl.StdSchedulerFactory;
 public class Main {
 
     public static void main(String[] args) throws SchedulerException {
+        // 创建调度器
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-
+        // 创建触发器
         CronTrigger cronTrigger = TriggerBuilder.newTrigger()
                 .withIdentity("trigger1", "group1")
                 .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
                 .build();
-
-
+        // 创建任务
         JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
                 .withIdentity("job1", "group1")
                 .build();
-
+        // 将任务和触发器注册到调度器中
         scheduler.scheduleJob(jobDetail, cronTrigger);
+        // 启动调度器
         scheduler.start();
     }
 }
